d25ad9505e9ad983d63a66fdcd1f7a7333e148a9
Neil Floyd <neilfloyd99@gmail.com>
2020-01-20 21:38:39 -0500

    firebaseConfig.js and firebaseInit.js added

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBKNNK_pUyHD4oIE5KGrky1wK6MR296K6A",
+    authDomain: "marketingtool-9547d.firebaseapp.com",
+    databaseURL: "https://marketingtool-9547d.firebaseio.com",
+    projectId: "marketingtool-9547d",
+    storageBucket: "marketingtool-9547d.appspot.com",
+    messagingSenderId: "507611107373",
+    appId: "1:507611107373:web:11fe681ab73a592a90a8a4",
+    measurementId: "G-5Z8XLFCHPB"
+}
